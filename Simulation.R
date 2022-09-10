a027e088ea381210d7b6bf4da38100886425e0bb
m-vidal <54290040+m-vidal@users.noreply.github.com>
2022-09-10 17:32:46 +0200

    Create Simulation.R

---
+++ Simulation.R
@@ -0,0 +1,17 @@
+#SIMULATION
+source("whiten.R")
+library(fda)
+
+witt <- c("PCA","PCA-cor","ZCA","ZCA-cor","Cholesky")
+Tab1 <- matrix(0,5,10)
+rownames(Tab1) <- c("PCA","PCA-cor","ZCA","ZCA-cor","CHOL")
+colnames(Tab1) <- c("ISR-B-13",
+                    "ISR-B-25",
+                    "trCROSS-B-13",
+                    "trCROSS-B-25",
+                    "CROSS2-B-13",
+                    "CROSS2-B-25",
+                    "trCORR-B-13",
+                    "trCORR-B-25",
+                    "CORR2-B-13",
+                    "CORR2-B-25")
